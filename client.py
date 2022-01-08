f81ec14d127e842a63176b33a279c43f69c69bd6
jrferguson <1q2w3e4r5t6y>
2022-01-08 18:12:57 +0000

    added authentication

--- client.py
+++ client.py
@@ -14,32 +14,71 @@ def Main():
         s.settimeout(5)   # 5 seconds
         print("Connection with server: OK")
 
+        print("Select one")
+        print("1 - create new user")
+        print("2 - Process Cvs")
+
+        menu_selection = input("Select operation to do: ")
+        if(menu_selection == "1"):
+            create_user(s)
+        else:
+            process_file(s)
         # input of the csv file name to process(hard-coded or by user input)
         # Filename = 'csv.txt'
-        Filename = input("Name of file to process: ")
-
-        s.send(Filename.encode('utf-8'))
-        s.shutdown(socket.SHUT_WR) 
-        data = s.recv(1024).decode('utf-8') 
-
-        try:
-            # received the data in string format, 
-            # so need to convert in a dict/json to easily read the result
-            json_acceptable_string = data.replace("'", "\"") 
-            result = json.loads(json_acceptable_string)
-
-            # printing of the result on screen
-            print('How many people in the list are male?  ' + str(result['males']))
-            print('How many people in the list are older than 30?  ' + str(result['above_threshold']))
-            print('How many employers are in Human Resources? ' + str(result['hr']))
-
-            # closing of the connection
-            s.close()
-        except Exception:
-            print("Could not process your request, please see the server logs")
+        
     except socket.error as exc:
         print("Connection with server: Failed")
         print ("Caught exception socket.error : %s" % exc)
+
+def create_user(s):
+    username = input("Please insert username: ")
+    password = input("Please insert password: ")
+    confirm_password = input("Please type again password: ")
+
+    if(username == "" and password == "" and password == confirm_password ):
+        print("Could not process credentials")
+    else:
+        data = {
+            "menu_selection": "1",
+            "username": username,
+            "password": password
+        }   
+        s.send(str.encode(json.dumps(data)))
+
+def authenticate(s):
+    username = input("Please insert username: ")
+    password = input("Please insert password: ")
+
+    request_data = {
+            "menu_selection": "2",
+            "username": username,
+            "password": password
+        }   
+    s.send(str.encode(json.dumps(request_data)))
+
+def process_file(s):
+    authenticate(s)
+    # file_name = input("Name of file to process: ")
+    # s.send(file_name.encode('utf-8'))
+    # s.shutdown(socket.SHUT_WR) 
+    # data = s.recv(1024).decode('utf-8') 
+
+    # try:
+    #     # received the data in string format, 
+    #     # so need to convert in a dict/json to easily read the result
+    #     json_acceptable_string = data.replace("'", "\"") 
+    #     result = json.loads(json_acceptable_string)
+
+    #     # printing of the result on screen
+    #     print('How many people in the list are male?  ' + str(result['males']))
+    #     print('How many people in the list are older than 30?  ' + str(result['above_threshold']))
+    #     print('How many employers are in Human Resources? ' + str(result['hr']))
+
+    #     # closing of the connection
+    #     s.close()
+    # except Exception:
+    #     print("Could not process your request, please see the server logs")
+
 
 if __name__ == '__main__':
     Main()
--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 import socket
 import json
+import hashlib
+import os
+import sqlite3
 
 # import processor function
 from csv_processor import process
@@ -8,8 +11,14 @@ from csv_processor import process
 general_error_message = 'File not found. Check the file name and try again.'
 file_not_found_error_message = 'Something went wrong while processing, please check the format and content of file'
 connection_error_message = 'Could not start up server, please contact the technical team'
+salt = os.urandom(32)
 
 def Main():
+
+    db_connection = sqlite3.connect('shows.db')
+    cursor = db_connection.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS users
+              (username TEXT, password TEXT)''')
     # connection settings
     host = '127.0.0.1'
     port = 65432
@@ -25,51 +34,97 @@ def Main():
         # This loop accepts a connection, then reads from the 
         # client until done
         while True:
-            filename = ''
             c, addr = s.accept()
             print("Connection from: " + str(addr))
-            # This loops keeps the server running until an EOF 
-            # (we've receive zero length string in python socket), this should happen
-            # when the client socket has been shutdown
-            while True:
-                data = c.recv(1024).decode('utf-8')
-                if not data:
-                    break
-                filename += data
-            print("File to process: " + filename)
+            data = c.recv(1024).decode('utf-8');
+            json_acceptable_string = data.replace("'", "\"") 
+            result = json.loads(json_acceptable_string)
+            menu_selection = str(result['menu_selection'])
+            username = result['username']
+            password = result['password']
 
-            # open the csv file in read mode (we could open in binary mode with 'rb' but to make
-            # the processing of the file we are opening it in read mode)
-            try:
-                myfile = open(filename, "r")
+            if(menu_selection == "1"):
+                create_user(db_connection, cursor, username, password)
+            if(menu_selection == "2"):
+                authenticate_user(cursor, username, password)
 
-                try:
-                    # call to the process function in the csv_processor.py file
-                    result = process(myfile)
-
-                    # encoding the result in a binary object using the json library in order to send the data
-                    # back the client in bytes-like object format
-                    encoded_result = json.dumps(result, indent=2).encode('utf-8')
-                    c.send(encoded_result)
-
-                    # closing of the connection
-                    c.close()
-                except Exception:
-                    # file processing error handling
-                    print(general_error_message)
-                    c.send(general_error_message.encode()) 
-                    c.close;
-            except OSError as os:
-                print(os)
-                # file not found error handling
-                print (file_not_found_error_message)
-                c.send(file_not_found_error_message.encode())
-                c.close;
     except socket.error:
         # connection error handling 
         print(connection_error_message)
         c.send(file_not_found_error_message.encode())
-        c.close;
+        c.close
+
+
+def create_user(db_connection, cursor, username, password):    
+    rows = get_user_by_username(cursor, username)   
+    if(len(rows) > 0):
+        print("user already exists")
+    else:    
+        hashed_password = hash_pw(password) 
+        cursor.execute('''INSERT INTO users (username, password) VALUES(?,?)''', (username, hashed_password))
+        db_connection.commit()
+        print("user created successfully")
+
+def authenticate_user(cursor, username, password):     
+    users = get_user_by_username(cursor, username)
+    if(len(users) == 0):
+        print("user already exists")
+    else:
+        user = users[0]    
+        user_password = user[1]
+        if(user_password == hash_pw(password)):
+            print("user logged successfully")
+            return True
+        else:
+            print("user logged failed") 
+            return False 
+
+def get_user_by_username(cursor, username):
+    cursor.execute("SELECT * FROM users WHERE username=?", [username])
+    return cursor.fetchall()
+
+def hash_pw(password):
+    return hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt ,100000, dklen=128) 
+
+def process_file(c):
+    filename = ''
+    # This loops keeps the server running until an EOF 
+    # (we've receive zero length string in python socket), this should happen
+     # when the client socket has been shutdown
+    while True:
+        data = c.recv(1024).decode('utf-8')
+        if not data:
+            break
+        filename += data
+        print("File to process: " + filename)
+
+        # open the csv file in read mode (we could open in binary mode with 'rb' but to make
+        # the processing of the file we are opening it in read mode)
+        try:
+            myfile = open(filename, "r")
+            try:
+                # call to the process function in the csv_processor.py file
+                result = process(myfile)
+
+                # encoding the result in a binary object using the json library in order to send the data
+                # back the client in bytes-like object format
+                encoded_result = json.dumps(result, indent=2).encode('utf-8')
+                c.send(encoded_result)
+
+                # closing of the connection
+                c.close()
+            except Exception:
+                # file processing error handling
+                print(general_error_message)
+                c.send(general_error_message.encode()) 
+                c.close;
+        except OSError as os:
+            print(os)
+            # file not found error handling
+            print (file_not_found_error_message)
+            c.send(file_not_found_error_message.encode())
+            c.close
+
 
 if __name__ == '__main__':
     Main()
